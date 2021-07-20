fd7f9ac533226dfac86bc7c4fb626fe1e2c3db40
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-20 10:01:11 +0300

    first commmit

---
+++ react/01-helloworld/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+	  return (
+		<div className="App">
+			<h2>Hello World</h2>
+		</div>
+	  );
+  }
+}
+
+export default App;

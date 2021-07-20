d1f79cc93e369cb3e567623f70d14c3a3a1624df
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-20 11:05:23 +0300

    stateful component

--- react/01-helloworld/src/App.js
+++ react/01-helloworld/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import HelloWorld from './HelloWorld';
 
 class App extends React.Component {
   render() {
 	  return (
 		<div className="App">
 			<h2>Hello World</h2>
+			<HelloWorld/>
+			<HelloWorld name="Erno"/>
 		</div>
 	  );
   }

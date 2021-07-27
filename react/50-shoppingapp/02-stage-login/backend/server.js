1c88d89c07eb36f854efa1a2f2d58342c5ecab93
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-27 13:26:50 +0300

    login

--- react/50-shoppingapp/02-stage-login/backend/server.js
+++ react/50-shoppingapp/02-stage-login/backend/server.js
@@ -73,6 +73,44 @@ app.post("/register",function(req,res) {
 	})
 });
 
+app.post("/login",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request 1"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad Request 2"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request 3"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(registeredUsers[i].username === req.body.username) {
+			bcrypt.compare(req.body.password,registeredUsers[i].password,function(err,success) {
+				if(err) {
+					return res.status(400).json({message:"Bad Request 4"})
+				}
+				if(!success) {
+					return res.status(401).json({message:"Unauthorized"})
+				}
+				let token = createToken();
+				if(!token) {
+					return res.status(400).json({message:"Bad Request 5"})
+				}
+				let now = Date.now();
+				let session = {
+					user:req.body.username,
+					ttl:now+time_to_live_diff,
+					token:token
+				}
+				loggedSessions.push(session);
+				return res.status(200).json({token:token});
+			})
+			return;
+		}
+	}
+	return res.status(401).json({message:"Unauthorized"});
+});
+
 app.use("/api",isUserLogged,apiroute);
 
 app.listen(port);

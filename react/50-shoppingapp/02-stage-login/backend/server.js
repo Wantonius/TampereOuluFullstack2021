d1dcdec05e6d6ad6c7e56d83c98994191f9ded2a
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-27 13:03:07 +0300

    isuserlogged

--- react/50-shoppingapp/02-stage-login/backend/server.js
+++ react/50-shoppingapp/02-stage-login/backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const apiroute = require("./routes/apiroute");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 let app = express();
 
@@ -7,7 +9,71 @@ app.use(express.json());
 
 let port = process.env.PORT || 3001
 
-app.use("/api",apiroute);
+//LOGIN DATABASES
+
+let registeredUsers = [];
+let loggedSessions = [];
+let time_to_live_diff = 3600000;
+
+//MIDDLEWARE
+
+createToken = () => {
+	let token = crypto.randomBytes(128);
+	return token.toString("hex");
+}
+
+isUserLogged = (req,res,next) => {
+	if(!req.headers.token) {
+		return res.status(403).json({message:"forbidden"});
+	}
+	for(let i=0;i<loggedSessions.length;i++) {
+		if(req.headers.token === loggedSessions[i].token) {
+			let now = Date.now();
+			if(now > loggedSessions[i].ttl) {
+				loggedSessions.splice(i,1);
+				return res.status(403).json({message:"forbidden"});
+			}
+			loggedSessions[i].ttl = now + time_to_live_diff;
+			req.session = {};
+			req.session.user = loggedSessions[i].user;
+			return next();
+		}
+	}
+	return res.status(403).json({message:"forbidden"});
+}
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request 1"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad Request 2"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request 3"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({message:"Username is already in use"})
+		}			
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			return res.status(400).json({message:"Bad request 4"});
+		}
+		let user = {
+			username:req.body.username,
+			password:hash
+		}
+		registeredUsers.push(user);
+		console.log(registeredUsers);
+		return res.status(201).json({message:"User registered"});
+	})
+});
+
+app.use("/api",isUserLogged,apiroute);
 
 app.listen(port);
 

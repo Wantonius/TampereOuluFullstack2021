bbab2ac2a6504f73ae92ff19385677b9c7ca20cd
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-28 10:23:36 +0300

    login

--- react/50-shoppingapp/02-stage-login/frontend/src/App.js
+++ react/50-shoppingapp/02-stage-login/frontend/src/App.js
@@ -69,8 +69,37 @@ class App extends React.Component {
 		}
 	}
 	
-	login = (user) => {
-		
+	login = async (user) => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		let response = await fetch("/login",request).catch(error => {
+			console.log("There was an error:"+error)
+		});
+		if(!response) {
+			return;
+		}
+		if(response.ok) {
+			let data = await response.json().catch(error => {
+				console.log("Error parsing json:"+error);
+			});
+			if(!data) {
+				console.log("Failed to parse json")
+				return;
+			}
+			this.setState({
+				isLogged:true,
+				token:data.token
+			}, () => {
+				this.saveToStorage();
+				this.getList();
+			})
+		} else {
+			console.log("Server responded with a status:"+response.status)
+		}			
 	}
 	
 	//REST API

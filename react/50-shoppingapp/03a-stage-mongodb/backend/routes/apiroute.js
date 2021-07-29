504b52f204df6673c427df31de060ea89a3775b5
Erno Hentonen <erno.hentonen@opiframe.com>
2021-07-29 13:30:32 +0300

    mongodb works

--- react/50-shoppingapp/03a-stage-mongodb/backend/routes/apiroute.js
+++ react/50-shoppingapp/03a-stage-mongodb/backend/routes/apiroute.js
@@ -45,36 +45,38 @@ router.post("/shopping",function(req,res) {
 })
 
 router.delete("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id,10);
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === tempId) {
-			if(database[i].user !== req.session.user) {
-				return res.status(401).json({message:"You are not authorized to remove this object"});
-			}
-			database.splice(i,1);
-			return res.status(200).json({message:"success!"})
+	itemModel.deleteOne({"_id":req.params.id,"user":req.session.user}, function(err){
+		if(err) {
+			console.log("Failed to remove item id:"+req.params.id+". Reason:",err)
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"not found!"})
+		return res.status(200).json({message:"success"});
+	})
 })
 
 router.put("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id,10);
+	if(!req.body) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	if(!req.body.type) {
+		return res.status(400).json({message:"Bad request"});
+	}
 	let item = {
-		...req.body,
-		id:tempId,
-		user:req.session.user
+		user:req.session.user,
+		type:req.body.type.toLowerCase(),
+		count:req.body.count,
+		price:req.body.price
 	}
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === tempId) {
-			if(database[i].user !== req.session.user) {
-				return res.status(401).json({message:"You are not authorized to edit this object"});
-			}			
-			database.splice(i,1,item);
-			return res.status(200).json({message:"success!"})
+	itemModel.replaceOne({"_id":req.params.id,"user":req.session.user},item,function(err,response){
+		if(err) {
+			console.log("Failed to edit item id:"+req.params.id+". Reason:",err)
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"not found!"})
+		if(!response.nModified) {
+			return res.status(404).json({message:"not found"})
+		}
+		return res.status(200).json({message:"success"});
+	})
 })
 
 module.exports = router;

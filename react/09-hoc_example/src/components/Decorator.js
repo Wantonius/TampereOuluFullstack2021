4905ada87fef1b3072bf4c6e61ca7b4a4ff157bc
Erno Hentonen <erno.hentonen@opiframe.com>
2021-08-04 12:31:45 +0300

    hoc example

---
+++ react/09-hoc_example/src/components/Decorator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Decorator = (Component) => {
+	return class extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				color:"red"
+			}
+		}
+		
+		onChange = (event) => {
+			this.setState({
+				[event.target.name]:event.target.value
+			})
+		}
+		
+		render() {
+			return(
+				<div>
+					<Component {...this.props} color={this.state.color}/>
+					<br/>
+					<input type="text"
+							name="color"
+							onChange={this.onChange}
+							value={this.state.color}/>
+				</div>	
+			)
+		}
+		
+	}
+}
+
+export default Decorator;

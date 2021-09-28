be99b99848c019c09239a4c1b6bf177ff2d860e1
Erno Hentonen <erno.hentonen@opiframe.com>
2021-09-28 10:58:52 +0300

    angular first stuff

---
+++ angular/directives/src/app/personlist.component.ts
@@ -0,0 +1,411 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector:"personlist",
+	templateUrl:"./personlist.component.html"
+})
+export class PersonList {
+	list = [
+	{
+		"firstname": "Hashim",
+		"lastname": "Lucas"
+	},
+	{
+		"firstname": "Angelica",
+		"lastname": "Williamson"
+	},
+	{
+		"firstname": "Portia",
+		"lastname": "Strong"
+	},
+	{
+		"firstname": "Alexa",
+		"lastname": "Haley"
+	},
+	{
+		"firstname": "Haley",
+		"lastname": "Becker"
+	},
+	{
+		"firstname": "Tamara",
+		"lastname": "Duncan"
+	},
+	{
+		"firstname": "Dale",
+		"lastname": "Sloan"
+	},
+	{
+		"firstname": "Xyla",
+		"lastname": "Hyde"
+	},
+	{
+		"firstname": "Madeline",
+		"lastname": "Moon"
+	},
+	{
+		"firstname": "Demetria",
+		"lastname": "Mcintyre"
+	},
+	{
+		"firstname": "Sopoline",
+		"lastname": "Vang"
+	},
+	{
+		"firstname": "Illiana",
+		"lastname": "Wheeler"
+	},
+	{
+		"firstname": "Ethan",
+		"lastname": "Delgado"
+	},
+	{
+		"firstname": "Francesca",
+		"lastname": "Watts"
+	},
+	{
+		"firstname": "Zena",
+		"lastname": "Knapp"
+	},
+	{
+		"firstname": "Brent",
+		"lastname": "Wiley"
+	},
+	{
+		"firstname": "Leo",
+		"lastname": "Washington"
+	},
+	{
+		"firstname": "Jana",
+		"lastname": "Ewing"
+	},
+	{
+		"firstname": "Yael",
+		"lastname": "Terry"
+	},
+	{
+		"firstname": "Kylee",
+		"lastname": "Gill"
+	},
+	{
+		"firstname": "Eugenia",
+		"lastname": "Yang"
+	},
+	{
+		"firstname": "Georgia",
+		"lastname": "Abbott"
+	},
+	{
+		"firstname": "Hope",
+		"lastname": "Skinner"
+	},
+	{
+		"firstname": "Kameko",
+		"lastname": "Sexton"
+	},
+	{
+		"firstname": "Hedwig",
+		"lastname": "Beard"
+	},
+	{
+		"firstname": "Martin",
+		"lastname": "Horton"
+	},
+	{
+		"firstname": "Lucas",
+		"lastname": "Guzman"
+	},
+	{
+		"firstname": "Vivien",
+		"lastname": "Chandler"
+	},
+	{
+		"firstname": "Clayton",
+		"lastname": "Hodge"
+	},
+	{
+		"firstname": "Hedwig",
+		"lastname": "Lucas"
+	},
+	{
+		"firstname": "Brady",
+		"lastname": "Carver"
+	},
+	{
+		"firstname": "Chantale",
+		"lastname": "Weeks"
+	},
+	{
+		"firstname": "Idona",
+		"lastname": "Blevins"
+	},
+	{
+		"firstname": "Marvin",
+		"lastname": "Barron"
+	},
+	{
+		"firstname": "Phillip",
+		"lastname": "Schwartz"
+	},
+	{
+		"firstname": "Alexander",
+		"lastname": "Goodman"
+	},
+	{
+		"firstname": "Cassandra",
+		"lastname": "Harrington"
+	},
+	{
+		"firstname": "Tanek",
+		"lastname": "Hale"
+	},
+	{
+		"firstname": "Leigh",
+		"lastname": "Huber"
+	},
+	{
+		"firstname": "Kaden",
+		"lastname": "Bradford"
+	},
+	{
+		"firstname": "Tamara",
+		"lastname": "Odom"
+	},
+	{
+		"firstname": "Mikayla",
+		"lastname": "Parrish"
+	},
+	{
+		"firstname": "Jayme",
+		"lastname": "Stark"
+	},
+	{
+		"firstname": "Wang",
+		"lastname": "Kidd"
+	},
+	{
+		"firstname": "Stephanie",
+		"lastname": "Zimmerman"
+	},
+	{
+		"firstname": "Aphrodite",
+		"lastname": "Sampson"
+	},
+	{
+		"firstname": "Aimee",
+		"lastname": "Ferrell"
+	},
+	{
+		"firstname": "Kermit",
+		"lastname": "Sloan"
+	},
+	{
+		"firstname": "Cassandra",
+		"lastname": "Fisher"
+	},
+	{
+		"firstname": "Fulton",
+		"lastname": "Humphrey"
+	},
+	{
+		"firstname": "Stacy",
+		"lastname": "Sutton"
+	},
+	{
+		"firstname": "Wyoming",
+		"lastname": "Walker"
+	},
+	{
+		"firstname": "Giacomo",
+		"lastname": "Schultz"
+	},
+	{
+		"firstname": "Hilda",
+		"lastname": "Maddox"
+	},
+	{
+		"firstname": "Penelope",
+		"lastname": "Ward"
+	},
+	{
+		"firstname": "Fredericka",
+		"lastname": "Ramsey"
+	},
+	{
+		"firstname": "Neville",
+		"lastname": "Frank"
+	},
+	{
+		"firstname": "Stacy",
+		"lastname": "Bartlett"
+	},
+	{
+		"firstname": "Alana",
+		"lastname": "Lang"
+	},
+	{
+		"firstname": "Mechelle",
+		"lastname": "Hubbard"
+	},
+	{
+		"firstname": "Cole",
+		"lastname": "Allison"
+	},
+	{
+		"firstname": "Beatrice",
+		"lastname": "Ochoa"
+	},
+	{
+		"firstname": "Yasir",
+		"lastname": "Chase"
+	},
+	{
+		"firstname": "Imani",
+		"lastname": "Meyers"
+	},
+	{
+		"firstname": "Geoffrey",
+		"lastname": "Ballard"
+	},
+	{
+		"firstname": "Howard",
+		"lastname": "Phelps"
+	},
+	{
+		"firstname": "Murphy",
+		"lastname": "Bond"
+	},
+	{
+		"firstname": "Kameko",
+		"lastname": "Wolf"
+	},
+	{
+		"firstname": "Hop",
+		"lastname": "Garner"
+	},
+	{
+		"firstname": "Claudia",
+		"lastname": "Dejesus"
+	},
+	{
+		"firstname": "Jolie",
+		"lastname": "Lewis"
+	},
+	{
+		"firstname": "Slade",
+		"lastname": "Finch"
+	},
+	{
+		"firstname": "Allen",
+		"lastname": "Blackwell"
+	},
+	{
+		"firstname": "Xyla",
+		"lastname": "Russell"
+	},
+	{
+		"firstname": "Xenos",
+		"lastname": "Gentry"
+	},
+	{
+		"firstname": "Kadeem",
+		"lastname": "French"
+	},
+	{
+		"firstname": "Kelly",
+		"lastname": "Rich"
+	},
+	{
+		"firstname": "Kathleen",
+		"lastname": "Short"
+	},
+	{
+		"firstname": "Sharon",
+		"lastname": "Norton"
+	},
+	{
+		"firstname": "Amos",
+		"lastname": "Frye"
+	},
+	{
+		"firstname": "Cade",
+		"lastname": "Harrell"
+	},
+	{
+		"firstname": "Steel",
+		"lastname": "Hurley"
+	},
+	{
+		"firstname": "Emerald",
+		"lastname": "Kline"
+	},
+	{
+		"firstname": "Bevis",
+		"lastname": "Blackburn"
+	},
+	{
+		"firstname": "Martena",
+		"lastname": "Slater"
+	},
+	{
+		"firstname": "Wade",
+		"lastname": "Mcfarland"
+	},
+	{
+		"firstname": "Cheryl",
+		"lastname": "Frank"
+	},
+	{
+		"firstname": "Ruby",
+		"lastname": "Robertson"
+	},
+	{
+		"firstname": "Maia",
+		"lastname": "Bullock"
+	},
+	{
+		"firstname": "Ulric",
+		"lastname": "Weeks"
+	},
+	{
+		"firstname": "Reece",
+		"lastname": "Pena"
+	},
+	{
+		"firstname": "Brett",
+		"lastname": "Christensen"
+	},
+	{
+		"firstname": "Garth",
+		"lastname": "Shepherd"
+	},
+	{
+		"firstname": "Abbot",
+		"lastname": "Mckee"
+	},
+	{
+		"firstname": "Malachi",
+		"lastname": "Chapman"
+	},
+	{
+		"firstname": "Fletcher",
+		"lastname": "Spencer"
+	},
+	{
+		"firstname": "Pascale",
+		"lastname": "Rios"
+	},
+	{
+		"firstname": "Aspen",
+		"lastname": "Grimes"
+	},
+	{
+		"firstname": "Xanthus",
+		"lastname": "Baxter"
+	},
+	{
+		"firstname": "Adara",
+		"lastname": "Bishop"
+	}
+]
+	
+}

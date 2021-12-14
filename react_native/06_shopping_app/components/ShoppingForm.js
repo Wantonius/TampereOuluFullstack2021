daf7a41a5409d65000f5810b7350fef90a5e6f3d
Erno Hentonen <erno.hentonen@opiframe.com>
2021-12-14 12:30:15 +0200

    brackets

--- react_native/06_shopping_app/components/ShoppingForm.js
+++ react_native/06_shopping_app/components/ShoppingForm.js
@@ -3,6 +3,107 @@ import {View,Text,Pressable,TextInput,StyleSheet} from 'react-native';
 
 const ShoppingForm = (props) => {
 
+	const [state,setState] = useState({
+		type:"",
+		count:0,
+		price:0
+	})
+	
+	addToList = () => {
+		let item = {
+			...state,
+			id:0
+		}
+		props.addToList(item);
+		setState({
+			type:"",
+			count:0,
+			price:0			
+		})
+	}
+
+	return(
+		<View style={styles.container}>
+			<View style={styles.row}>
+				<Text style={[styles.text,styles.label]}>Type:</Text>
+				<TextInput style={[styles.text,styles.input]}
+					onChangeText={(text) => setState((state) => {
+						return {
+							...state,
+							type:text
+						}
+					})
+					
+					} value={state.type}/>
+			</View>
+			<View style={styles.row}>
+				<Text style={[styles.text,styles.label]}>Count:</Text>
+				<TextInput style={[styles.text,styles.input]}
+					onChangeText={(text) => setState((state) => {
+						return {
+							...state,
+							count:text
+						}
+					})				
+					} value={state.count} keyboardType="numeric"/>
+			</View>
+			<View style={styles.row}>
+				<Text style={[styles.text,styles.label]}>Price:</Text>
+				<TextInput style={[styles.text,styles.input]}
+					onChangeText={(text) => setState((state) => {
+						return {
+							...state,
+							price:text
+						}
+					})				
+					} value={state.price} keyboardType="numeric"/>
+			</View>
+			<View style={styles.buttonRow}>
+				<Pressable style={styles.addButton} 
+					onPress={addToList}>
+					<Text style={styles.text}>Add</Text>
+				</Pressable>
+			</View>
+		</View>
+	)
 }
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		alignItems:"center",
+		justifyContent:"center",
+	},
+	row:{
+		flex:1,
+		flexDirection:"row",
+		alignItems:"center",
+		justifyContent:"space-between"
+	},
+	buttonRow:{
+		flex:1,
+		flexDirection:"row",
+		alignItems:"center",
+		justifyContent:"center"
+	},
+	addButton:{
+		height:80,
+		width:110,
+		backgroundColor:"blue",
+		alignItems:"center",
+		justifyContent:"center"
+	},
+	text:{
+		fontFamily:"sans-serif",
+		fontSize:18
+	},
+	label:{
+		fontWeight:"bold"
+	},
+	input:{
+		width:200,
+		backgroundColor:"lightgreen"
+	}
+})
 
 export default ShoppingForm;
--- react_native/06_shopping_app/components/ShoppingList.js
+++ react_native/06_shopping_app/components/ShoppingList.js
@@ -3,6 +3,38 @@ import {FlatList,View,Pressable,Text,StyleSheet} from 'react-native';
 
 const ShoppingList = (props) => {
 
+	return(
+		<View style={styles.container}>
+			<View style={styles.buttonBox}>
+				<Pressable style={styles.navigateButton} onPress={() => props.navigation.navigate("Add Item")}>
+					<Text style={styles.textStyle}>Add New Item</Text>
+				</Pressable>
+			</View>
+			<View style={styles.listBox}>
+				<FlatList data={props.list}
+							renderItem={
+								({item}) => {
+									return(
+										<View style={styles.row}>
+											<Text style={styles.textStyle}>
+											Type:{item.type}</Text>
+											<Text style={styles.textStyle}>
+											Count:{item.count}</Text>
+											<Text style={styles.textStyle}>
+											Price:{item.price}</Text>
+											<Pressable 
+											style={styles.buttonStyle} 
+											onPress={() => props.removeFromList(item.id)}>
+												<Text style={styles.textStyle}>Remove</Text>
+											</Pressable>
+										</View>
+									)
+								}
+							}
+						keyExtractor={item => ""+item.id}
+					/>
+			</View>
+	)
 }
 
 export default ShoppingList;
